d807175f4ac5abdea458e67a12cbefdb7770746f
Maulana Bintang Irfansyah <77730390+maulanabin@users.noreply.github.com>
2022-03-04 08:25:29 +0700

    Tugas2_Eksekusi_SourceCode

---
+++ Tugas2_Mengeksekusi_SourceCode/Tugas2_MaulanaBintangIrfansyah.php
@@ -0,0 +1,6 @@
+ <!-- Tugas 2 - Maulana Bintang Irfansyah - JWD -->
+<?php 
+	echo "Selamat Datang <br>";
+	echo "Ayo kita belajar Junior Web Developer bersama <br>";
+	echo "di BPPTIK Cikarang";
+?>
---
+++ Tugas2_Mengeksekusi_SourceCode/latihan1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Junior Web Developer</title>
+</head>
+<body>
+    <p>Kalimat ini dibuat menggunakan HTML.</p>
+    <?php
+        echo "<p> Kalimat ini dibuat menggunakan PHP. </p>";
+    ?>
+</body>
+</html>
